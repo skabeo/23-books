1f882ca783e0b87b0b886c9ee01966e1834bae01
skabeo <spencerokyere95@gmail.com>
2023-08-07 21:45:25 +0000

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :categories
   has_many :details
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
 end
