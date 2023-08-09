dc3d65aa5dfbd737070e2f585f22c7712417834c
skabeo <spencerokyere95@gmail.com>
2023-08-09 15:51:58 +0000

    Implement hamburger icon logic

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.querySelector('.hamburger');
+  const menuContainer = document.querySelector('.menu-container');
+  const closeButton = document.querySelector('.close-button');
+
+  hamburger.addEventListener('click', function () {
+    menuContainer.style.display = 'block';
+  });
+
+  closeButton.addEventListener('click', function () {
+    menuContainer.style.display = 'none';
+  });
+});
+
