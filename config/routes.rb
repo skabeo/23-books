c9c76351b9bd4767645fdce3e8baa167da057068
skabeo <spencerokyere95@gmail.com>
2023-08-09 07:23:50 +0000

    Add authenticated users route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  authenticated :user do
+    root to: "categories#index", as: :authenticated_root
+  end
   root "home#index"
   resources :categories, only: [:index, :show, :new, :create, :destroy, :update] do
-    resources :tranactions, only: [:index, :new, :create, :destroy, :edit, :update]
+    resources :details, only: [:index, :new, :create, :destroy, :edit, :update]
   end
 end
