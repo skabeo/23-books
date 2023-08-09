44d547f8e9c97bdeb5808f0be961754ab031d6d2
skabeo <spencerokyere95@gmail.com>
2023-08-09 12:18:26 +0000

    Generate test files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
@@ -60,4 +61,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  Capybara.default_driver = :selenium_chrome
 end
