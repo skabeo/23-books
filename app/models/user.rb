97a58c954f27b76424bc85490873117918b38525
skabeo <spencerokyere95@gmail.com>
2023-08-09 09:12:35 +0000

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :firstname, presence: true, length: { in: 1..15 }
+  validates :surname, presence: true, length: { in: 1..15 }
 end
