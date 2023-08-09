d103d0b8a03618d1aa1932a3beacb6eace04eeed
skabeo <spencerokyere95@gmail.com>
2023-08-09 07:22:29 +0000

    Modify categrory controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,15 @@ class CategoriesController < ApplicationController
   before_action :authenticate_user!
   def index
     @categories = current_user.categories.order(created_at: :desc).all
+    @total_amount = []
+
+    @categories.each do |category|
+      total = 0
+      category.category_details.each do |category_detail|
+        total += category_detail.detail.amount
+      end
+      @total_amount.push(total)
+    end
   end
 
   def new
